fc7e1c7aa7ca9e26f5a2c0f585e723c9b3f6df26
chubbymaus <court@loftydev.com>
2018-02-01 05:00:09 -0700

    added first and last name fields to user forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :main_nav
+  before_action :configure_permitted_params, if: :devise_controller?
   def main_nav
     @topics = Topic.all
   end
     
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end  
 end
