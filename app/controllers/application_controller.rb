732203f31bbe68c25a85ed4d1606cd9e684547b9
chubbymaus <court@loftydev.com>
2018-02-02 18:39:03 -0700

    added installation requirements for petergate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :main_nav
   before_action :configure_permitted_params, if: :devise_controller?
+  layout :set_layout
   def main_nav
     @topics = Topic.all
   end
     
+
+ def set_layout
+    if current_user
+      'content'
+    else
+      'application'
+    end
+ end  
   def configure_permitted_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
