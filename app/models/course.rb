1a6e83a2831cc0ae842bf391cd585ec469911c47
chubbymaus <court@loftydev.com>
2018-02-09 01:22:12 -0700

    fixed topics dropdown to only show topics with courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,5 +5,5 @@ class Course < ApplicationRecord
   validates_presence_of :title, :thumb_image, :description, :topic_id
 
   belongs_to :topic
-  has_many :guides
+  has_many :guides, dependent: :destroy
 end
